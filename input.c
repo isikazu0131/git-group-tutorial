09372a6623942e288db24b61a51c63aaab97aa6e
rucchi <ant7.1728@gmail.com>
2016-06-23 17:59:50 +0900

    inputとoutput追加

--- input.c
+++ input.c
@@ -1,15 +1,21 @@
 #include "input.h"
 
 /*
-  IDを入力させ，その値を戻り値とする
+IDを入力させ，その値を戻り値とする
 */
 int scan_id(){
-
+	int id;
+	printf("IDを入力");
+	scanf("%d", &id);
+	return id;
 }
 
 /*
-  パスワードを入力させ，その値を戻り値とする
+パスワードを入力させ，その値を戻り値とする
 */
 int scan_pass(){
-
+	int pw;
+	printf("Passwordを入力");
+	scanf("%d", &pw);
+	return pw;
 }
